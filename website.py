eeb03c7a1669f91a0ec683730ebc36e3c58be2ed
Erika <erika12994@gmail.com>
2025-01-22 16:26:12 -0500

    Added a certifications page

--- website.py
+++ website.py
@@ -21,6 +21,9 @@ def projects():
 def resume():
     return redirect("https://www.dropbox.com/scl/fi/rha1xx9mnmdqsfy67q89f/resume_erika_avila.pdf?rlkey=v7nw9wg2kzqripi5kiz9ahw9f&dl=0")
 
+@app.route("/certifications")
+def certifications():
+    return render_template('certifications.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
